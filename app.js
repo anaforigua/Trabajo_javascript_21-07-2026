a2517077b9d52fba118d29102520b8d25cf5a535
ana <anamariaforigua20@gmail.com>
2026-07-21 11:10:26 -0500

    actualizar repo con cambio y edicion  de un get llamado hi pero con uns frase iconica de un telenovela

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express"; //ESM
-import {configDotenv} from "dotenv"
-configDotenv()
-
+//import {configDotenv} from "dotenv"
+//configDotenv()
+import "dotenv/config"
 
 const app = express();
 const puerto = process.env.PORT||3050;
@@ -9,8 +9,12 @@ const puerto = process.env.PORT||3050;
 app.get ("/",(req,res)=>{
     res.send("hola ficha 3407180 del SENA ")
 
-})
+});
+
+app.get("/hi",(req,res)=>{
+    res.send(`<p> Recuerda querida: ''Todos los hombres son iguales,mas no sus sueldos''. Paola Bracho</p><h1> La usurpadora </h1>`);
+});
 
 app.listen( puerto,()=>{
     console.log (`servidor funcionando!! en el puerto ${puerto}`)
-})
+});
