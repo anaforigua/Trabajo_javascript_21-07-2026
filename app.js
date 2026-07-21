5841a9f46616bb11c58323fea54ae5ac8ee2cc80
ana <anamariaforigua20@gmail.com>
2026-07-21 08:59:39 -0500

    subir repo con ejercicio javascript sin el package lock json por el momento

---
+++ app.js
@@ -0,0 +1,13 @@
+//import express from "express"
+const express = require("express")
+const app = express()
+const puerto = 3000
+
+app.get ("/",(req,res)=>{
+    res.send("hola ficha 3407180")
+
+})
+
+app.listen( puerto,()=>{
+    console.log ("servidor funcionando")
+})
