71e6085c65a679587840ec40f4b5a77a6eaf6450
ana <anamariaforigua20@gmail.com>
2026-07-21 11:32:11 -0500

    Agregar otro enpoint con mi nombre y ficha donde la variabler es mi nombre

--- app.js
+++ app.js
@@ -15,8 +15,19 @@ app.get("/hi",(req,res)=>{
     res.send(`<p> Recuerda querida: ''Todos los hombres son iguales,mas no sus sueldos''. Paola Bracho</p><h1> La usurpadora </h1> ${hi}`);
 });
 
+app.get("/liz/:amor",(req,res)=>{
+    const amor = req.params.amor
+    res.send(`<p> Mi corazon nene, mi corazon nene, mi corazon muriendose de amor ${amor} </p>`);
+});
+
+app.get("/nombre/:Anamaria",(req,res)=>{
+    const Anamaria = req.params.Anamaria
+    res.send(`<p> Hola soy de la ficha 3407180 me llamo ${Anamaria} </p>`);
+});
+
 app.listen( puerto,()=>{
     console.log (`SERVIDOR https//localhost:${puerto} 
         https//127.0.0.1:${puerto}`)
     
 });
+
