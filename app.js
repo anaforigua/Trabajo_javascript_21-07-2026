b0da9f7bec395829dffbcb3a1b8e6e984e2aed4e
ana <anamariaforiguamurcia@gmail.com>
2026-07-30 18:37:47 -0500

    subir nuev actualizacion de al repo de respaldo

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.get ("/",(req,res)=>{
 });
 
 app.get("/hi",(req,res)=>{
+    const hi = req.params.hi
     res.send(`<p> Recuerda querida: ''Todos los hombres son iguales,mas no sus sueldos''. Paola Bracho</p><h1> La usurpadora </h1> ${hi}`);
 });
 
@@ -34,7 +35,7 @@ app.listen( puerto,()=>{
 // 
 app.get("/productos/:categoria",(req,res)=>{
     const mi_categoria = req.params.categoria
-    res.send(`Hola escribe l acategoria de un producto que te interese ${mi_categoria} `);
+    res.send(`Hola escribe la categoria de un producto que te interese ${mi_categoria} `);
 });
 
 app.get("/productos/:categoria/:id",(req,res)=>{
@@ -49,8 +50,110 @@ app.get("/libros/:isbn", (req,res)=> {
     res.send(`<h1>Hola coloca el ISBN (el codigo del libro)</h1> ${mi_isbn}`)
 })
 
-app.get("/productos/:lacteos?fecha_v", (req,res)=> {
+app.get("/productos/:lacteos", (req,res)=> {
     const lacteo= req.params.lacteos
     const fecha_v= req.query.fecha_v
-    res.send(`<h1>Hola coloca el ISBN (el codigo del libro)</h1> ${lacteo}`)
+    res.send(`<h1>Hola coloca una categoria de interes </h1> ${lacteo} <p>Fecha de vencimiento: ${fecha_v}</p>`
+    )
 })
+
+
+
+
+const lista_productos=[
+    
+    { id: 1, nombre: "Laptop", categoria: "Tecnología", precio: 2500000 },
+    { id: 2, nombre: "Mouse", categoria: "Tecnología", precio: 80000 },
+    { id: 3, nombre: "Camiseta", categoria: "Ropa", precio: 50000 },
+    { id: 4, nombre: "Zapatos", categoria: "Calzado", precio: 180000 }
+    
+];
+
+app.get("/productos", (req, res) => {
+   const {nombre}=req.query;
+       if (nombre) {
+        const resultado = lista_productos.filter(
+            producto => producto.nombre.toLowerCase() === nombre.toLowerCase()
+        );
+         return res.json(resultado);
+    }
+
+    res.json(lista_productos);
+});
+
+app.put("/productos", (req, res) => {
+    const { id, nombre, precio } = req.query;
+
+    const producto = lista_productos.find(
+        producto => producto.id === Number(id)
+    );
+
+    if (!producto) {
+        return res.status(404).json({
+            mensaje: "Producto no encontrado"
+        });
+    }
+
+    if (nombre) {
+        producto.nombre = nombre;
+    }
+
+    if (precio) {
+        producto.precio = Number(precio);
+    }
+
+    res.json({
+        mensaje: "Producto actualizado",
+        producto: producto
+    });
+});
+
+
+
+const lista_libros=[
+    {
+         ISBN:21345,titulo:"El viejo y el mar", autor:"Ernest Hemingway."},
+         {
+         ISBN:21346,titulo:"Carrie", autor:"Stephen King."},
+         {
+         ISBN:22430,titulo:"El mensajero de agartha zombies", autor:"Mario Mendoza."}
+]
+
+app.get("/libros", (req, res) => {
+   const {titulo}=req.query;
+       if (titulo) {
+        const resultado = lista_libros.filter(
+            libro => libro.titulo.toLowerCase() === titulo.toLowerCase()
+        );
+         return res.json(resultado);
+    }
+
+    res.json(lista_libros);
+});
+
+app.put("/libros", (req, res) => {
+    const { ISBN, titulo, autor } = req.query;
+
+    const libro = lista_libros.find(
+        libro => libro.ISBN === Number(ISBN)
+    );
+
+    if (!libro) {
+        return res.status(404).json({
+            mensaje: "Libro no encontrado"
+        });
+    }
+
+    if (titulo) {
+        libro.titulo = titulo;
+    }
+
+    if (autor) {
+        libro.autor = autor;
+    }
+
+    res.json({
+        mensaje: "Libro actualizado correctamente",
+        libro: libro
+    });
+});
